6a3f490026d69bc776e08094b32ee1f85f1e303e
Udbhavi Manmohan Shetty <bhavishetty1234@gmail.com>
2026-06-27 12:21:21 +0530

    Updated project files

--- schemas/job.py
+++ schemas/job.py
@@ -4,7 +4,14 @@ from typing import Optional
 class JobCreate(BaseModel):
     title: str
     salary: int
+    description: Optional[str] = None
+    company_id: int
 
-class JobUpdate(BaseModel):
+class JobUpdate(JobBase):
+    pass
+
+class JobUpdate(JobBase):
     title: Optional[str] = None
     salary: Optional[int] = None
+    description: Optional[str] = None
+    company_id: Optional[int] = None
