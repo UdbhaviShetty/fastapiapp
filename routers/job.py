6e65e52661b90feafb81a48e38100a2ad69ae815
Udbhavi Manmohan Shetty <bhavishetty1234@gmail.com>
2026-06-29 10:32:28 +0530

    Updated project files

--- routers/job.py
+++ routers/job.py
@@ -1,24 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,HTTPException,Depends,status
 from schemas.job import JobCreate, JobUpdate
+from models.job import Job
+from sqlalchemy.orm import Session
+from database import get_db
 
 router = APIRouter(prefix="/job", tags=["job"])
-jobs = []
 
-@router.post("/")
-def create_job(job: JobCreate):
-    jobs.append(job)
+@router.post("/",status_code=status.HTTP_201_CREATED,response_model=jobResponse)
+def create_job(job: JobCreate,db:Session=Depends(get_db)):
+    db_job = Job(**job.dict())
+    db.add(db_job)
+    db.commit()
+    db.refresh(db_job)
+    return db_job
+
+@router.get("/",status_code=status.HTTP_200_OK,response_model=list[JobResponse])
+def get_all_job(db:Session=Depends(get_db)):
+    jobs = db.query(Job).all()
     return jobs
 
-@router.get("/")
-def get_all_job():
-    return jobs
-
-@router.get("/{job_id}")
-def get_job(job_id: int):
-    return jobs[job_id]
-
-@router.get("/{job_id}")
-def get_job(job_id: int):
+@router.get("/{job_id}",status_code=status.HTTP_200_OK,response_model=JobResponse)
+def get_job(job_id: int,db:Session=Depends(get_db)):
     return jobs[job_id]
 
 @router.put("/{job_id}")
