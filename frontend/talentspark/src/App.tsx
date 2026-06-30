95156acc70036052a5dd90769e4590599605106b
Udbhavi Manmohan Shetty <bhavishetty1234@gmail.com>
2026-06-30 13:43:39 +0530

    Updated project files

--- frontend/talentspark/src/App.tsx
+++ frontend/talentspark/src/App.tsx
@@ -3,50 +3,50 @@ import NavBar from "./components/NavBar";
 import CompanyCard from "./components/CompanyCard";
 import JobCard from "./components/JobCard";
 import Footer from "./components/Footer";
-import {useEffect,useState} from "react";
-import { getCompanies } from "./Services/Companyservice";
-import type {Company} from "./types/company";
+import {useEffect, useState} from "react";
+import { getCompanies } from "./Services/CompanyService";
+import type { Company } from "./types/Company";
 
-function App(){
-  const [loading,setLoading] = useState(true);
-  const [error,setError] = useState<Error | null>(null)
-  const [companies,setCompanies] = useState<Company[]>([]);
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+  const [companies, setCompanies] = useState<Company[]>([]);
 
   async function fetchCompanies() {
     setLoading(true);
+
     try {
       const companies = await getCompanies();
       setCompanies(companies);
-    } catch (err) {
-      setError(Error);
-    } finally {
+  } catch (error: any) {
+      setError(error);
+  } finally {
       setLoading(false);
-    }
   }
+}
 
   useEffect(() => {
     fetchCompanies();
   }, []);
 
-  if(loading){
-    return <div>Loading...</div>
+  if (loading) {
+    return <div>Loading...</div>;
   }
 
-  if(error){
-    return <div>Error: {error.message}</div>
+  if (error) {
+    return <div>Error: {error.message}</div>;
   }
 
-  return(
-    <>
-    <NavBar />
-    <Welcome />
-    <br />
-    <CompanyCard key={companies.id}
-    companies={companies}/>
-    <JobCard />
-    <Footer />
-    </>
-  )
+  return (
+    <div>
+      <NavBar />
+      <Welcome />
+      <CompanyCard />
+      <JobCard />
+      <Footer />
+    </div>
+  );
 }
 
-export default App  
+export default App
+
--- frontend/talentspark/src/components/CompanyCard.tsx
+++ frontend/talentspark/src/components/CompanyCard.tsx
@@ -1,6 +1,6 @@
 import { getCompanies } from "../Services/CompanyService";
 import { useEffect, useState } from "react";
-import type { Company } from "../types/company";
+import type { Company } from "../types/Company";
 
 function CompanyCard() {
     const [companies, setCompanies] = useState<Company[]>([]);
@@ -15,18 +15,18 @@ function CompanyCard() {
     }, []);
 
     return (
-        <div>
-            {companies.map((company) => (
-                <div key={company.id}>
-                    <h1>{company.name}</h1>
-                    <p>Email:{company.email}</p>
-                    <p>Phone: {company.phone}</p>
-                    <p>Location: {company.location}</p>
-                    <hr></hr>
-                </div>
-            ))}
-        </div>
-    )
+    <div>
+        {companies.map((company) => (
+            <div key={company.id}>
+                <h1>{company.name}</h1>
+                <p>Email: {company.email}</p>
+                <p>Phone: {company.phone}</p>
+                <p>Location: {company.location}</p>
+                <hr></hr>
+            </div>
+        ))}
+    </div>
+)
 }
 
-export default CompanyCard
+export default CompanyCard;
