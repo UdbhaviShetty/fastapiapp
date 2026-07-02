d7f3788a05ef2b24f526e37104be9a7b07cc62f4
Udbhavi Manmohan Shetty <bhavishetty1234@gmail.com>
2026-07-02 14:18:02 +0530

    newbranch

--- backend/schemas/users.py
+++ backend/schemas/users.py
@@ -1,16 +1,19 @@
 from pydantic import BaseModel
 
 class UserBase(BaseModel):
-    name: str
-    email: str
-    password: str
-    role: str
+        name: str
+        email: str
+        password: str
+        role: str
 
 class UserCreate(UserBase):
     pass
 
-class UserResponse(UserBase):
+class UserResponse(BaseModel):
     id: int
+    name: str
+    email: str
+    role: str
 
     class Config:
         from_attributes = True
